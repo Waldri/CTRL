a87f2a80e88ea2a8a5857d1703a49e07995c4d12
Waldri Oliveira <waldri.so@gmail.com>
2023-11-06 16:16:46 +0000

    core

---
+++ green.py
@@ -0,0 +1,38 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.core.window import Window
+
+class BlueScreenApp(App):
+    def build(self):
+        # Define a cor de fundo para azul
+        Window.clearcolor = (0, 1, 0, 1)
+        
+        # Crie um layout com um rótulo centralizado
+        layout = BoxLayout(orientation='vertical')
+        label = Label(text="Tela Cheia Verde")
+        layout.add_widget(label)
+        
+        return layout
+
+if __name__ == '__main__':
+    BlueScreenApp().run()
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.core.window import Window
+
+class BlueScreenApp(App):
+    def build(self):
+        # Define a cor de fundo para azul
+        Window.clearcolor = (0, 0, 1, 1)
+        
+        # Crie um layout com um rótulo centralizado
+        layout = BoxLayout(orientation='vertical')
+        label = Label(text="Tela Cheia Azul")
+        layout.add_widget(label)
+        
+        return layout
+
+if __name__ == '__main__':
+    BlueScreenApp().run()
